ac6b7703f35bc57af96f790b37aaf35b2db75636
Thiemann96 <erictg96@googlemail.com>
2023-09-12 11:39:07 +0200

    compartmentalized some functions

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -1,6 +1,16 @@
 "use client";
 import MapComponent from "@/components/Map/Map";
-import { PauseIcon, PlayIcon } from "@heroicons/react/24/outline";
+import {
+  BeakerIcon,
+  ChartBarIcon,
+  CloudIcon,
+  PauseIcon,
+  PlayIcon,
+  RssIcon,
+  SignalIcon,
+  SunIcon,
+  TruckIcon,
+} from "@heroicons/react/24/outline";
 import {
   Dialog,
   DialogContent,
@@ -13,10 +23,24 @@ import { Settings2Icon, SettingsIcon } from "lucide-react";
 import { useState } from "react";
 import { DialogTrigger } from "@radix-ui/react-dialog";
 import SettingsModal from "@/components/Map/Settings";
+import RecordButton from "@/components/Map/RecordButton";
+import MeasurementsOverview from "@/components/Map/MeasurementsOverview";
 
 export default function Home() {
   const [recording, setRecording] = useState(false);
 
+  const [data, setData] = useState({
+    temperature: 0,
+    humidity: 0,
+    pm1: 0,
+    pm25: 0,
+    pm4: 0,
+    pm10: 0,
+    accelerationX: 0,
+    accelerationY: 0,
+    accelerationZ: 0,
+    speed: 0,
+  });
   const toggleRecording = () => {
     setRecording(!recording);
   };
@@ -26,22 +50,10 @@ export default function Home() {
       <MapComponent />
       <div className="flex flex-col items-center">
         <div className="absolute left-5 top-20 bg-slate-100 rounded-lg">
-          <div className="flex flex-col ">
-            <span>31°C</span>
-            <span>55%</span>
-            <span>1/2/3/4 µg/m³</span>
-            <span>240/234/129 m²/s</span>
-            <span>22km/h</span>
-            <span>55cm</span>
-          </div>
+          <MeasurementsOverview data={data} />
         </div>
         <div className="absolute top-20 right-5 rounded-lg">
-          <div
-            className={cx(
-              "rounded-full w-6 h-6",
-              recording ? "bg-green-500" : "bg-red-500",
-            )}
-          ></div>
+          <RecordButton recording={recording} />
         </div>
         <div className="absolute bottom-20 right-5 rounded-lg flex gap-2 flex-col bg-white">
           {recording ? (
