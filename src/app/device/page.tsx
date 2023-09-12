60c5a4c9982136dae5a1bfc9663f2506229dcd4b
Felix Erdmann <hello@felixerdmann.com>
2023-09-12 10:35:39 +0200

    feat: add modal to preview incoming data

--- src/app/device/page.tsx
+++ src/app/device/page.tsx
@@ -1,7 +1,10 @@
+import PreviewModal from "@/components/Device/PreviewModal";
+
 export default function Home() {
   return (
     <div className="flex w-full flex-col h-full">
       <h1> DEVICE PAGE </h1>
+      <PreviewModal />
     </div>
   );
 }
