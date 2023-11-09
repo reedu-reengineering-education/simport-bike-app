cd0ea72fccc6f2a2e5116219174e52289de50e97
Felix Erdmann <hello@felixerdmann.com>
2023-11-09 19:29:13 +0100

    fix: set refresh token and debounce request

--- src/app/AuthChecker.tsx
+++ src/app/AuthChecker.tsx
@@ -4,28 +4,33 @@ import { getUser } from '@/lib/api/openSenseMapClient'
 import { useAuthStore } from '@/lib/store/useAuthStore'
 import { App } from '@capacitor/app'
 import { useEffect } from 'react'
+import debounce from 'lodash.debounce'
 
 export default function AuthChecker() {
   const { setIsLoggedIn } = useAuthStore()
+
+  const debouncedAuthCheck = debounce(async () => {
+    try {
+      const userData = await getUser()
+      if (userData) setIsLoggedIn(true)
+      else setIsLoggedIn(false)
+    } catch (e) {
+      setIsLoggedIn(false)
+    }
+  }, 200)
 
   useEffect(() => {
     App.addListener('appStateChange', async ({ isActive }) => {
       if (isActive) {
         // if we can get user data, we are logged in
-        try {
-          const userData = await getUser()
-          if (userData) setIsLoggedIn(true)
-          else setIsLoggedIn(false)
-        } catch (e) {
-          setIsLoggedIn(false)
-        }
+        await debouncedAuthCheck()
       }
     })
 
     return () => {
       App.removeAllListeners()
     }
-  }, [setIsLoggedIn])
+  }, [])
 
   return null
 }
