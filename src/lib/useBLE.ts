47f8ddb308e6acc5b93cb1cb0ca82fbff9004a58
Felix Erdmann <hello@felixerdmann.com>
2023-12-07 15:35:38 +0100

    feat: connect with name via camera

--- src/lib/useBLE.ts
+++ src/lib/useBLE.ts
@@ -29,21 +29,35 @@ export default function useBLEDevice(options: RequestBleDeviceOptions) {
       if (name) {
         device = await new Promise(async (resolve, reject) => {
           try {
+            toast({
+              variant: 'default',
+              title: 'Suche nach Gerät...',
+              description: name,
+            })
+            const timeout = setTimeout(async () => {
+              await BleClient.stopLEScan()
+              reject('Timeout')
+            }, 10000)
             await BleClient.requestLEScan(
               {
                 name,
               },
               result => {
+                console.log(result)
+                clearTimeout(timeout)
                 resolve(result.device)
               },
             )
           } catch (e) {
+            console.log(e)
             reject(e)
           }
         })
       } else {
         device = await BleClient.requestDevice(options)
       }
+
+      console.log(device)
 
       await BleClient.connect(device.deviceId, () => {
         toast({
