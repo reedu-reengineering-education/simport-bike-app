7609a4ec2b19c50cccdb7b164b9390aab4ea3717
Felix Erdmann <hello@felixerdmann.com>
2024-07-12 14:15:07 +0200

    feat: create sensebox:bike as drawer

--- src/components/Wizard/CreateBikeBoxDialog.tsx
+++ src/components/Wizard/CreateBikeBoxDialog.tsx
@@ -1,11 +1,12 @@
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 import { createSenseBoxBike } from '@/lib/api/openSenseMapClient'
 import { senseBoxBikeModel } from '@/lib/api/opensensemap-bike-model-factory'
 import { useAuthStore } from '@/lib/store/useAuthStore'
@@ -86,71 +87,75 @@ export default function CreateBikeBoxDialog() {
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button variant={'secondary'} className="w-fit">
           <Plus className="mr-2 w-5" /> {t('trigger')}
         </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="mb-2">{t('title')}</DialogTitle>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-8 text-left"
-            >
-              <FormField
-                control={form.control}
-                name="model"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{t('model')}</FormLabel>
-                    <Select
-                      value={field.value}
-                      name={field.name}
-                      onValueChange={field.onChange}
-                    >
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue onBlur={field.onBlur} ref={field.ref} />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="default">{t('default')}</SelectItem>
-                        <SelectItem value="atrai">{t('atrai')}</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{t('name')}</FormLabel>
+      </DrawerTrigger>
+      <DrawerContent className="p-4">
+        <DrawerHeader>
+          <DrawerTitle className="mb-2">{t('title')}</DrawerTitle>
+        </DrawerHeader>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 text-left"
+          >
+            <FormField
+              control={form.control}
+              name="model"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t('model')}</FormLabel>
+                  <Select
+                    value={field.value}
+                    name={field.name}
+                    onValueChange={field.onChange}
+                  >
                     <FormControl>
-                      <Input placeholder="senseBox:bike" {...field} />
+                      <SelectTrigger>
+                        <SelectValue onBlur={field.onBlur} ref={field.ref} />
+                      </SelectTrigger>
                     </FormControl>
-                    <FormDescription>{t('description')}</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <DialogDescription className="flex items-center">
-                <InfoIcon className="mr-2 h-5 md:mr-4" />
-                {t('position-info')}
-              </DialogDescription>
+                    <SelectContent>
+                      <SelectItem value="default">{t('default')}</SelectItem>
+                      <SelectItem value="atrai">{t('atrai')}</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t('name')}</FormLabel>
+                  <FormControl>
+                    <Input placeholder="senseBox:bike" {...field} />
+                  </FormControl>
+                  <FormDescription>{t('description')}</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DrawerDescription className="flex items-center">
+              <InfoIcon className="mr-2 h-5 md:mr-4" />
+              {t('position-info')}
+            </DrawerDescription>
+
+            <DrawerFooter className="pb-4">
               <Button type="submit" disabled={loading}>
                 {loading && <Loader2 className="mr-2 w-5 animate-spin" />}
                 {t('create')}
               </Button>
-            </form>
-          </Form>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+            </DrawerFooter>
+          </form>
+        </Form>
+      </DrawerContent>
+    </Drawer>
   )
 }
