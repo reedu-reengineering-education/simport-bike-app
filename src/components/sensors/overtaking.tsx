5b447b5a7f69b0f793d5e4c5bbb82d392664c60f
PaulaScharf <paulascharf@gmx.de>
2024-09-04 13:54:37 +0200

    differentiate between overtaking car and bike

--- src/components/sensors/overtaking.tsx
+++ src/components/sensors/overtaking.tsx
@@ -1,24 +1,31 @@
 import OvertakingPredictionSensor from '@/lib/sensors/overtaking-prediction'
 import { Sensor } from '.'
 import SensorView from './sensor-view'
+import i18n from '@/i18n'
 
 const overtaking: Sensor = {
   uuid: OvertakingPredictionSensor.BLE_CHARACTERISTIC,
   Component: (
     <SensorView
       characteristic={OvertakingPredictionSensor.BLE_CHARACTERISTIC}
-      rawValueToValue={value => (value.measurement[0] as number) * 100}
+      rawValueToValue={({ measurement }) => [
+        measurement[0] as number * 100,
+        measurement[1] as number * 100
+      ]}
       rawHistoryValuesToData={values =>
         values.map(v => ({
           x: v.timestamp,
-          y: (v.measurement[0] as number) * 100,
+          overtaking_car: (v.measurement[0] as number) * 100,
+          overtaking_bike: (v.measurement[1] as number) * 100,
         }))
       }
       name={'overtaking'}
       unit={'%'}
+      labels={[i18n.t('phenomena.overtaking_car'), i18n.t('phenomena.overtaking_bike')]}
       chartProps={{
         index: 'x',
-        categories: ['y'],
+        categories: ['overtaking_car', 'overtaking_bike'],
+        colors: ['blue', 'amber'],
         maxValue: 100,
         minValue: 0,
       }}
--- src/lib/sensors/overtaking-prediction.tsx
+++ src/lib/sensors/overtaking-prediction.tsx
@@ -1,7 +1,8 @@
 import AbstractSensor from './abstract-sensor'
 import BaseSensor from './base-sensor'
+import i18n from '@/i18n'
 
-export default class OvertakingPredictionSensor<T extends [number]>
+export default class OvertakingPredictionSensor<T extends [number, number]>
   extends BaseSensor<T>
   implements AbstractSensor<T>
 {
@@ -9,10 +10,11 @@ export default class OvertakingPredictionSensor<T extends [number]>
     'FC01C688-2C44-4965-AE18-373AF9FED18D'
 
   public static type: string = 'overtaking'
+  public static attributes: string[] = [i18n.t('phenomena.overtaking_car'), i18n.t('phenomena.overtaking_bike')]
 
   parseData(data: DataView): T {
-    const [prediction] = this.parsePackages(data)
+    const [overtaking_car, overtaking_bike] = this.parsePackages(data)
 
-    return [prediction] as T
+    return [overtaking_car, overtaking_bike] as T
   }
 }
